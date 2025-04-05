3d72fb190dfe4c489df93f367838d38db6e4ea69
Armunshi <munshiabdulrazzaq@gmail.com>
2025-04-05 16:55:44 +0530

    changed the url endpoints for the request

--- flask_todo_frontend/src/App.jsx
+++ flask_todo_frontend/src/App.jsx
@@ -1,66 +1,77 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
 import './App.css'
 
-const APP_URL = "https://flask-to-do-backend.onrender.com" 
+const APP_URL = "https://flask-to-do-backend.onrender.com"
 
 function App() {
 
-  const [todos,setTodos] = useState([])
-  const [task,setTask] = useState("");
+  const [todos, setTodos] = useState([])
+  const [task, setTask] = useState("")
 
   useEffect(() => {
-    axios.get(APP_URL).then((res) => setTodos(res.data));
-  }, []);
+    axios.get(`${APP_URL}/todos`)
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.error("Error fetching todos:", err))
+  }, [])
 
-  const deleteTask=(id)=>{
-    console.log(id)
-    axios.delete(`${APP_URL}/${id}`).then(()=>{
-      setTodos(todos.filter(todo=>todo.id !==id ))
-    })
+  const deleteTask = (id) => {
+    axios.delete(`${APP_URL}/todos/${id}`)
+      .then(() => {
+        setTodos(todos.filter(todo => todo.id !== id))
+      })
+      .catch((err) => console.error("Error deleting todo:", err))
   }
-  const toggleDone=(id)=>{
-    console.log(id)
-    axios.put(`${APP_URL}/${id}`).then(()=>{
-      setTodos(todos.map((todo)=>{todo.id=id?{...todo,done:!todo.done}:todo}) )
-    })
+
+  const toggleDone = (id) => {
+    const todo = todos.find(t => t.id === id)
+    axios.put(`${APP_URL}/todos/${id}`, { done: !todo.done })
+      .then(() => {
+        setTodos(todos.map(t =>
+          t.id === id ? { ...t, done: !t.done } : t
+        ))
+      })
+      .catch((err) => console.error("Error updating todo:", err))
   }
-  const addTask= ()=>{
-    axios.post(APP_URL,{task}).then(()=>{
-      setTodos([...todos,{task,done:false}])
-      console.log(todos);
-      setTask("")
-    })
-  }  
+
+  const addTask = () => {
+    if (task.trim() === "") return
+    axios.post(`${APP_URL}/todos`, { task })
+      .then((res) => {
+        setTodos([...todos, { id: res.data.id || Date.now(), task, done: false }])
+        setTask("")
+      })
+      .catch((err) => console.error("Error adding todo:", err))
+  }
+
   return (
     <>
-    <h1>Todos</h1>
-    <div>
-      <input
-      type='text'
-      value={task} 
-      onChange={(e)=>setTask(e.target.value)}
-      placeholder='enter the task'
-      ></input>
-      <button onClick={addTask}>Add Task</button>
-  </div>
-    <ul>
-      {todos.map((todo)=>(
-        <li key={todo.id}>
-          <span
-          style={{textDecoration:todo.done?"line-through":"none",cursor:"pointer"}}
-          onClick={()=>toggleDone(todo.id)}>
-            {todo.task}
-          </span>
-          <button
-          onClick={()=>deleteTask(todo.id)}>
-            ❌
-          </button>
-        </li>
-      ))}
-    </ul>
-  <div>
-    </div>
+      <h1>Todos</h1>
+      <div>
+        <input
+          type='text'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder='Enter the task'
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.done ? "line-through" : "none",
+                cursor: "pointer"
+              }}
+              onClick={() => toggleDone(todo.id)}
+            >
+              {todo.task}
+            </span>
+            <button onClick={() => deleteTask(todo.id)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
