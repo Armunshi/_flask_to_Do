fe1f654feeb15e3950396a2bec3f7b151a48e10c
Armunshi <munshiabdulrazzaq@gmail.com>
2025-04-05 16:43:27 +0530

    allow cors properly

--- flask_todo_backend/app.py
+++ flask_todo_backend/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://flask-to-do-kcw6.onrender.com"], supports_credentials=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
