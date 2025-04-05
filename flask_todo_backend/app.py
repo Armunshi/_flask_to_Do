527b7d0d902c548ba99e8eda44c1318caef9d797
Armunshi <munshiabdulrazzaq@gmail.com>
2025-04-05 17:08:47 +0530

    backend 2

--- flask_todo_backend/app.py
+++ flask_todo_backend/app.py
@@ -5,15 +5,20 @@ import os
 
 app = Flask(__name__)
 
-CORS(app,
-     origins=["https://flask-to-do-kcw6.onrender.com"],
-     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-     supports_credentials=True)
+# Environment config (optional for local dev)
+# from dotenv import load_dotenv
+# load_dotenv()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# CORS setup (you can add localhost for dev too)
+CORS(app, resources={r"/todos*": {"origins": [
+    "https://flask-to-do-kcw6.onrender.com",
+    "http://localhost:3000"
+]}})
 
 # Model
 class Todo(db.Model):
@@ -21,29 +26,38 @@ class Todo(db.Model):
     task = db.Column(db.String(200), nullable=False)
     done = db.Column(db.Boolean, default=False)
 
-# Table creation
+# Create DB table
 with app.app_context():
     db.create_all()
 
-# Routes and functions
+# Root route (optional)
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Welcome to Flask ToDo API"}), 200
+
+# Todos route
 @app.route('/todos', methods=['GET', 'POST', 'OPTIONS'])
 def todos():
     if request.method == 'OPTIONS':
         return '', 200
-    elif request.method == 'GET':
+    
+    if request.method == 'GET':
         todos = Todo.query.all()
         return jsonify([{"id": t.id, "task": t.task, "done": t.done} for t in todos])
-    elif request.method == 'POST':
+
+    if request.method == 'POST':
         data = request.json
         new_todo = Todo(task=data['task'])
         db.session.add(new_todo)
         db.session.commit()
         return jsonify({"message": "To do added", "id": new_todo.id})
 
+# Delete route
 @app.route('/todos/<int:id>', methods=['DELETE', 'OPTIONS'])
-def delete_todos(id):
+def delete_todo(id):
     if request.method == 'OPTIONS':
         return '', 200
+
     todo = Todo.query.get(id)
     if todo:
         db.session.delete(todo)
@@ -51,10 +65,12 @@ def delete_todos(id):
         return jsonify({"message": "Task deleted"})
     return jsonify({"error": "Task not found"}), 404
 
+# Update route
 @app.route('/todos/<int:id>', methods=['PUT', 'OPTIONS'])
 def update_todo(id):
     if request.method == 'OPTIONS':
         return '', 200
+
     todo = Todo.query.get(id)
     if todo:
         todo.done = request.json.get('done', todo.done)
@@ -62,6 +78,7 @@ def update_todo(id):
         return jsonify({"message": "Task Updated"})
     return jsonify({"error": "Task not Found"}), 404
 
+# Run server
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
